ca774a5f6cd3897cb486e72dd74fdc50b45c5318
SergiumarK <s.marcocimd@gmail.com>
2023-12-20 15:26:09 +0300

    js_dinamic_slider

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const slider = document.getElementById("slider");
-const left = document.getElementById("left");
-const right = document.getElementById("right");
+const slider = document.getElementById("slider")
+const left = document.getElementById("left")
+const right = document.getElementById("right")
 
-let position = 0;
-const imageWidth = 700; // Asumăm că fiecare imagine are o lățime de 700px
-const numarTotalImagini = 3; // Înlocuiește cu numărul real de imagini
+let position = 0
+const imageSize = 700
+
+
+const sliderLength = document.querySelectorAll("#slider img").length
 
 left.addEventListener("click", () => {
-    position += imageWidth;
+    position += imageSize
     if (position > 0) {
-        // Dacă ne-am depășit la stânga, revenim la ultima imagine din dreapta
-        position = -(imageWidth * (numarTotalImagini - 1));
+        position = -(imageSize * (sliderLength - 1))
     }
-    slider.style.left = `${position}px`;
+    slider.style.left = `${position}px`
 });
 
 right.addEventListener("click", () => {
-    position -= imageWidth;
-    if (position < -(imageWidth * (numarTotalImagini - 1))) {
-        // Dacă ne-am depășit la dreapta, revenim la prima imagine din stânga
-        position = 0;
+    position -= imageSize
+    if (position < -(imageSize * (sliderLenght - 1))) {
+        position = 0
     }
-    slider.style.left = `${position}px`;
-});
+    slider.style.left = `${position}px`
+})
