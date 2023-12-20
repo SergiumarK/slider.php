dadfe6a9bd24dc5e263c806004d276f256e13f62
SergiumarK <s.marcocimd@gmail.com>
2023-12-20 14:50:09 +0300

    added_CSS

--- index.php
+++ index.php
@@ -7,9 +7,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Main Page</title>
+    <style>
+        <?php
+            include "styles.css";
+        ?>
+    </style>
+    <script defer src="script.js"></script>
 </head>
 <body>
+    <div id="slider-wrapper">
+        <div id="slider">
+            <?php
+                $sql = "SELECT * FROM slides";
+                $results = $conn->query($sql);
+                if ($results->num_rows > 0) {
+                    while ($row = $results->fetch_assoc()) {
+                        echo "<a class='slide' href=''>
+                            <img src='$row[url]' alt='Slide image'>
+                        </a>";
+                    }
+                }
+            ?>
+        </div>
+    </div>
     
 </body>
 </html>
